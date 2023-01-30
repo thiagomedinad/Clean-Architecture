4994b3b177d1b5bd45b64c6e912b1428728df589
thiagomedinad <thiagomedinad21@gmail.com>
2023-01-30 11:58:46 -0300

    test: ensure SignUpController returns 401 if passwords does not match

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,19 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { Unauthorized } from '../errors/unautorized-error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, unauthorized } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      return unauthorized(new Unauthorized())
     }
 
     return {
